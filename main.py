7fa24047f3784ce98fbd07934f07bcb02bc65ff8
Israalulu0 <israalulu0@gmail.com>
2022-09-28 10:08:33 +0300

    commit

---
+++ main.py
@@ -0,0 +1,9 @@
+list = [12, 75, 150, 180, 145, 525, 50]
+for number in list:
+    if number > 500:
+        break
+    elif number > 150:
+        continue
+    elif number % 5 == 0:
+        print(number)
+
